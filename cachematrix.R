e760e4ad68497aa932ce67a644ccca31c8a9925f
llkongs <k-johnny@qq.com>
2014-10-15 10:46:56 +0800

    Revert "Revert "Revert "Revert "Revert "Initial commit for Programming Assignment 2"""""
    This reverts commit bec22a4708cb6a1e4fe9c7f91bd88b9c7b2cc66e.

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,16 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Matrix inversion is usually a costly computation and their may be some 
+## benefit to caching the inverse of a matrix rather than compute it repeatedly
+## Now I've writen a pair of functions that cache the inverse of a matrix
 
-## Write a short comment describing this function
+
+##  This function creates a special "matrix" object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
 
 }
 
 
-## Write a short comment describing this function
+##  This function creates a special "matrix" object that can cache its inverse.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
